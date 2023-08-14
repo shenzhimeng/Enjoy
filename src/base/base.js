8fb24486dbb26a9956da8c911a7b557a0e3ec62f
0xBeWater <shenzhimeng@users.noreply.github.com>
2023-08-14 15:30:31 +0800

    Update base.js

--- src/base/base.js
+++ src/base/base.js
@@ -1,8 +1,17 @@
 const { ethers, Wallet, providers, utils, BigNumber } = require('ethers')
 
+const erc20abi = require('../json/ERC20ABI.json')
+
 function sleep(ms) {
     if (ms == 0) return
-    return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function getTokenBalance(wallet, tokenAddress) {
+    let contract = new ethers.Contract(tokenAddress, erc20abi, wallet)
+    let num = await contract.balanceOf(wallet.address)
+    return num.toString()
 }
 
 exports.sleep = sleep
+exports.getTokenBalance = getTokenBalance
